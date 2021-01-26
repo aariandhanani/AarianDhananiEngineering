94454872f3ffde7dcf39b689ae0ec46cafb8797e
aariandhanani <aariandhanani@gmail.com>
2021-01-26 15:58:31 -0600

    Changes

--- Program3/Program3.ino
+++ Program3/Program3.ino
@@ -9,35 +9,24 @@
 
 const int ledPin = 12;
 
-void setup() { // this is a function
+void setup()
+{ 
   /*the setup function only runs once
   when your turn on your arduino or you press the
   rest button
   We use it to initialize things
   */
-  pinMode(ledPin, OUTPUT)
-
-  digitalWrite 
+  
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, HIGH);
   Serial.begin(9600);
 }
 //another function that will repeat until you
 //turn off the arduino
-void loop() {
-  if(number >255)
-  {
-     number = 1; // between this to brackets is true
-  }
-  else
-  {
-    int result = 2*number+3;
-    Serial.println(result);//equation * mult + add / div - subs % modulus
-    number= number +1;
-    delay(100); //delay 100 milliseconds
-  }
-
-  for (int count = 0; count < 10; count++)
-  {
-    Serial.print("*");
-  }
-  Serial.println();
+void loop()
+{
+  digitalWrite(ledPin, HIGH);
+  delay(400);
+  digitalWrite(ledPin, LOW);
+  delay(400);
 }
