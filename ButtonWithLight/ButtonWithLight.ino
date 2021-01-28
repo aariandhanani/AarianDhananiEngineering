b762829cea145d9e3cf8bf380ed59304f817e64e
aariandhanani <aariandhanani@gmail.com>
2021-01-28 15:46:01 -0600

    Update ButtonWithLight.ino

--- ButtonWithLight/ButtonWithLight.ino
+++ ButtonWithLight/ButtonWithLight.ino
@@ -10,19 +10,23 @@ const int ledPin = 6;
 const int buttonPin = 5;
 byte readValue;
 
+// Setup
 void setup()
 { 
   Serial.begin(9600);
   pinMode(ledPin, OUTPUT); // This is an output
   pinMode (buttonPin, INPUT); // This is an input
-  digitalWrite(ledPin, HIGH);
 }
-//another function that will repeat until you
-//turn off the arduino
+
 void loop()
 {
-  digitalWrite(ledPin, HIGH);
-  delay(400);
-  digitalWrite(ledPin, LOW);
-  delay(400);
+  readValue = digitalRead(buttonPin);
+  if (readValue == 1)
+  {
+    digitalWrite(ledPin, HIGH);
+  }
+  else
+  {
+    digitalWrite(ledPin, LOW); 
+  }
 }
