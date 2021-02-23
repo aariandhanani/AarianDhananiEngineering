74a1d892d057ece37e74c933122982d151ae9ded
aariandhanani <aariandhanani@gmail.com>
2021-02-23 15:13:05 -0600

    Changes

--- HumiditySensor/HumiditySensor.ino
+++ HumiditySensor/HumiditySensor.ino
@@ -12,10 +12,20 @@ dht DHT;
 
 #define DHT11_PIN 7
 
+int controller = 0;
+const int ledPin1 = 4;
+const int ledPin2 = 5;
+const int ledPin3 = 6;
+const int fanPin = 12;
+
 // Setup
 void setup()
 { 
   Serial.begin(9600);
+  pinMode(ledPin1, OUTPUT); // This is an output
+  pinMode(ledPin2, OUTPUT); // This is an output
+  pinMode(ledPin3, OUTPUT); // This is an output
+  pinMode(fanPin, OUTPUT); // This is an output
 }
 
 void loop()
@@ -31,5 +41,39 @@ void loop()
   Serial.print(DHT.humidity);
   Serial.println(" is the humidity.");
 
+  if (temperatureVariable >= 100)
+  {
+    controller = 3;
+  }
+  else if (temperatureVariable >= 32)
+  {
+    controller = 2;
+  }
+  else
+  {
+    controller = 1;
+  }
+
+  switch (controller)
+  {
+    case 1:
+      digitalWrite(ledPin1, HIGH);
+      digitalWrite(ledPin2, LOW);
+      digitalWrite(ledPin3, LOW);
+      digitalWrite(fanPin, LOW);
+      break;
+    case 2:
+      digitalWrite(ledPin1, LOW);
+      digitalWrite(ledPin2, HIGH);
+      digitalWrite(ledPin3, LOW);
+      digitalWrite(fanPin, HIGH);
+      break;
+    case 3:
+      digitalWrite(ledPin1, LOW);
+      digitalWrite(ledPin2, LOW);
+      digitalWrite(ledPin3, HIGH);
+      digitalWrite(fanPin, HIGH);
+      break;
+  }
   delay(500);
 }
