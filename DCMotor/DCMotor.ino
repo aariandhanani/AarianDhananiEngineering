0258f4eabb772015a53fc4c62100ea883ca54f57
aariandhanani <aariandhanani@gmail.com>
2021-03-09 16:00:43 -0600

    Changes

--- DCMotor/DCMotor.ino
+++ DCMotor/DCMotor.ino
@@ -7,9 +7,17 @@
 const int pinDirection = 7;
 const int pinOnOff = 8;
 const int potentiometerPin = A0;
-const int direction1 = 2; //?
-const int direction2 = 5; //?
+const int direction1 = 2;
+const int direction2 = 3;
 const int motor = 9;
+
+boolean onOffValue;
+boolean onOffOld;
+boolean directionValue;
+boolean directionOld;
+
+boolean directionOn = false;
+boolean onOff = false;
 
 void setup() {
   Serial.begin(9600);
@@ -22,15 +30,62 @@ void setup() {
 
 }
 
+boolean reading (boolean last, int button)
+{
+  boolean current = digitalRead(button);
+  delay(5);
+  if (current != last)
+  {
+    current = digitalRead(button);
+  }
+  return current;
+}
+
 void loop() {
-//  Serial.println(digitalRead(pinDirection));
-//  Serial.println(digitalRead(pinDirection));
-    Serial.println(analogRead(potentiometerPin));
+//    onOff = digitalRead(pinOnOff);
+    onOffValue = reading(onOffOld, pinOnOff);
+    if (onOffValue == HIGH && onOffOld == LOW)
+    {
+      onOff = !onOff;
+    }
 
-    int value = analogRead(potentiometerPin);
 
-    value = map(value, 0, 1023, 0, 255);
-    digitalWrite(direction1, HIGH);
-    digitalWrite(direction2, LOW);
-    analogWrite(motor, value);
+
+//    directionOn = digitalRead(pinDirection);
+    directionValue = reading(directionOld, pinDirection);
+    if (directionValue == HIGH && directionOld == LOW)
+    {
+      directionOn = !directionOn;
+    }
+
+
+    
+//    Serial.println(digitalRead(pinDirection));
+//    Serial.println(digitalRead(pinOnOff));
+//    Serial.println(analogRead(potentiometerPin));
+
+    if (onOff == true)
+    {
+      if (directionOn == true)
+      {
+        digitalWrite(direction1, HIGH);
+        digitalWrite(direction2, LOW);
+      }
+      else
+      {
+        digitalWrite(direction1, LOW);
+        digitalWrite(direction2, HIGH);
+      }
+      int value = analogRead(potentiometerPin);
+
+      value = map(value, 0, 1023, 0, 255);
+      analogWrite(motor, value);
+    }
+    else
+    {
+      analogWrite(motor, LOW);
+    }
+
+    onOffOld = onOffValue;
+    directionOld = directionValue;
 }
