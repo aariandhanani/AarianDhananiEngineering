b35b5d51151594e9ba38bed1734a5cb6171e0ffa
aariandhanani <aariandhanani@gmail.com>
2021-03-31 15:14:20 -0500

    Edits

--- ShiftRegister/ShiftRegister.ino
+++ ShiftRegister/ShiftRegister.ino
@@ -7,14 +7,23 @@ const int SER = 8;
 const int LATCH = 9;
 const int CLCK = 12;
 
+int seq[] = {1,2,4,8,16,32,64,128};
+
 void setup() {
   pinMode(SER, OUTPUT);
   pinMode(LATCH, OUTPUT);
   pinMode(CLCK, OUTPUT);
+//  digitalWrite(LATCH, LOW);
+//  shiftOut(SER, CLCK, MSBFIRST, B11110000);
+//  digitalWrite(LATCH, HIGH);
 }
 
 void loop() {
-  digitalWrite(LATCH, LOW);
-  shiftOut(SER, CLCK, MSBFIRST, B11110000);
-  digitalWrite(LATCH, HIGH);
+  for (int i = 0; i < 8; i++)
+  {
+    digitalWrite(LATCH, LOW);
+    shiftOut(SER, CLCK, MSBFIRST, seq[i]);
+    digitalWrite(LATCH, HIGH);
+    delay(500);
+  }
 }
